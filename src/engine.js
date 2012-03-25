8dcce538601dd699018cce7ba00f77c8466c9285
unknown <morgan@.(none)>
2012-03-25 00:12:27 -0400

    Added text.

---
+++ src/engine.js
@@ -0,0 +1,42 @@
+// Constructs a sprite which updates and renders itself
+Sprite = function(_x, _y)
+{
+  return {
+    draw : function(self)
+    {
+      if(!self.image) { return; }
+      
+      context.drawImage(self.image, self.x - self.width / 2, self.y - self.height / 2);
+    },
+
+    update : function(self) { },
+
+    x: _x,
+    y: _y,
+    width: 0,
+    height: 0,
+    image: null,
+  }
+}
+
+TextSprite = function(_x, _y, _text)
+{
+  return {
+    draw : function(self)
+    {
+      if(!self.text) { return; }
+
+      context.font = self.font;
+      context.fillStyle = self.color;
+      context.fillText(self.text, self.x, self.y);
+    },
+
+    update : function(self) { },
+
+    x: _x,
+    y: _y,
+    text: _text,
+    font: "12pt helvetica",
+    color: "000000",
+  }
+}
--- src/game.js
+++ src/game.js
@@ -1,3 +1,9 @@
+// add "?date" to the end of a url to make sure stuff doesn't get cached
+nocache = "?" + (new Date()).getTime();
+
+// include the engine code
+document.write("<script type='text/javascript' src='src/engine.js"+nocache+"'></script>");
+
 // list of objects to draw and update
 scene = [];
 
@@ -5,35 +11,6 @@ scene = [];
 time = 0;
 //delta time
 dt = 0.02;
-
-// add "?date" to the end of a url to make sure stuff doesn't get cached
-nocache = "?" + (new Date()).getTime();
-
-
-// Constructs a sprite which updates and renders itself
-Sprite = function(x, y)
-{
-  return {
-    draw : function(self)
-    {
-      if(self.image)
-      {
-        context.drawImage(self.image, self.x - self.width / 2, self.y - self.height / 2);
-      }
-    },
-
-    update : function(self)
-    {
-
-    },
-
-    x: x,
-    y: y,
-    width: 0,
-    height: 0,
-    image: null,
-  }
-}
 
 // Called for each frame
 tick = function()
@@ -57,7 +34,6 @@ tick = function()
 // Set up all the objects
 initializeScene = function()
 {
-  
   bg = Sprite(0, 0);
   bg.image = new Image()
   bg.image.src = "assets/art/testBackground.png" + nocache;
@@ -67,14 +43,16 @@ initializeScene = function()
   player.image.src = "assets/art/boy.png" + nocache;
   player.width = 35;
   player.height = 95;
-
   player.update = function(self) {
     self.x += Math.sin(time * 2) * dt * 10;
     self.y -= dt * 5;
   }
 
+  text = TextSprite(3, 295, "hello world.");
+
   scene.push(bg);
   scene.push(player);
+  scene.push(text);
 }
 
 // Called onload by the host page
